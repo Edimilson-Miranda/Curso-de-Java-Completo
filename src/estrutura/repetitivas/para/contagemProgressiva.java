b9a20e6045f308c934529cc029247a5e7d6c4ea7
Edimilson Miranda <edimilsonsmp.464@gmail.com>
2025-09-24 11:24:33 -0300

    feat: adicionado os arquivo de estrutura para

---
+++ src/estrutura/repetitivas/para/contagemProgressiva.java
@@ -0,0 +1,9 @@
+package estrutura.repetitivas.para;
+
+public class contagemProgressiva {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
---
+++ src/estrutura/repetitivas/para/contagemRegressiva.java
@@ -0,0 +1,9 @@
+package estrutura.repetitivas.para;
+
+public class contagemRegressiva {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 0; i--){
+            System.out.println("Valor de i: " + i);
+        }
+    }
+}
