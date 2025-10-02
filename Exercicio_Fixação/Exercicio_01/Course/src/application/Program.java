6dc3865d355cebac159c7c89b6999abd5de44daa
Edimilson Miranda <edimilsonsmp.464@gmail.com>
2025-10-02 07:53:08 -0300

    Docs: inserido os enuciado do exercicio e paramentro

--- Exercicio_Fixação/Exercicio_01/Course/src/application/Program.java
+++ Exercicio_Fixação/Exercicio_01/Course/src/application/Program.java
@@ -4,17 +4,33 @@ import entities.Rectangle;
 
 import java.util.Locale;
 import java.util.Scanner;
+/*
+    Fazer um programa para ler os valores da largura e altura de retângulo.
+    Em seguida, mostrar na tela o valor de sua área, perímentro e diagonal.
+    Usar uma classe como mostrado.
+------------------------------------------
+               Rectangle
+ ------------------------------------------
+             -Width: double
+             -Height: double
+------------------------------------------
+             +Area(): double
+             +Perimeter(): double
+             +Diagonal(): double
+-------------------------------------------
+ */
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Rectangle rectangle = new Rectangle();
-        System.out.println("Enter rectangle width and height: ");
+        Rectangle rectangle = new Rectangle(); // chamado a classe Rectangle.
+        System.out.println("Enter rectangle width and height: "); // solicitando  a entrada de dados do programar
         rectangle.width = sc.nextDouble();
         rectangle.height = sc.nextDouble();
 
+        // formatando a saída de dados para ter duas casa decimal após o ponto.
         System.out.printf("AREA = %.2f%n", rectangle.area());
         System.out.printf("PERIMETER = %.2f%n", rectangle.perimeter());
         System.out.printf("DIAGONAL = %.2f%n", rectangle.diagonal());
--- Exercicio_Fixação/Exercicio_01/Course/src/entities/Rectangle.java
+++ Exercicio_Fixação/Exercicio_01/Course/src/entities/Rectangle.java
@@ -1,7 +1,5 @@
 package entities;
 
-import javax.xml.namespace.QName;
-
 public class Rectangle {
 
     public double width;
