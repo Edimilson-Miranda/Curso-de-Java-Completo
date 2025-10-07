9645be647b0ab6e1cd65addaa9c54e1a3dfc998e
Edimilson Miranda <edimilsonsmp.464@gmail.com>
2025-10-07 11:06:29 -0300

    feat: Versao três com método estático, no programa principal é só chama metodo com nome da própria classe

--- Exercicio_Fixação/Exercicio_04/src/util/Calculator.java
+++ Exercicio_Fixação/Exercicio_04/src/util/Calculator.java
@@ -2,12 +2,12 @@ package util;
 
 public class Calculator {
 
-    public final double PI = 3.14159;
+    public static final double PI = 3.14159;
 
-    public final double circumference(double radius){
+    public static final double circumference(double radius){
             return 2.0 * PI * radius;
     }
-    public final double volume(double radius){
+    public static final double volume(double radius){
         return 4.0 * PI * radius * radius * radius / 3.0;
     }
 }
