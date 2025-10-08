a4c7ef1b0388803645254672641199458364f2cf
Edimilson Miranda <edimilsonsmp.464@gmail.com>
2025-10-08 10:10:16 -0300

    feat: Melhorando a classe Product, com um construtor que exije que o usuario passa um parametro para instancia não ser null

--- Course_Product/Course/src/entities/Product.java
+++ Course_Product/Course/src/entities/Product.java
@@ -7,6 +7,13 @@ public class Product {
     public int quantity;
 
 
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+
     public double totalValueInStock(){
         return price * quantity;
     }
